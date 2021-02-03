0d9e40cb499732342e5f9e2c466a6b88db7affee
lhk <huikang.li@ambergroup.io>
2021-02-03 10:32:01 +0800

    update

--- src/ws.ts
+++ src/ws.ts
@@ -2,7 +2,6 @@ import { V3WebsocketClient } from '@okfe/okex-node';
 import * as moment from 'moment';
 import config from './config/index';
 const wss = new V3WebsocketClient(config.websocekHost);
-import dingding from './dingding';
 
 export function run() {
   wss.connect();
@@ -13,9 +12,12 @@ wss.on('open', () => {
     wss.login(config.httpkey, config.httpsecret, config.passphrase);
 });
 
+const symbol = 'LTC-USDT';
+const future = '210205';
+
 async function afterLogin() {
     // 订阅
-    wss.subscribe('futures/ticker:BTC-USDT-210326', 'swap/ticker:BTC-USDT-SWAP', 'swap/funding_rate:BTC-USDT-SWAP');
+    wss.subscribe(`futures/ticker:${symbol}-${future}`, `swap/ticker:${symbol}-SWAP`, `swap/funding_rate:${symbol}-SWAP`);
     // wss.subscribe('swap/candle300s:BTC-USD-SWAP');
 }
 
@@ -63,35 +65,31 @@ function tableMsg(marketData){
         futureInfo.best_ask = marketData.data[0].best_ask; // 卖一价
         futureInfo.best_bid = marketData.data[0].best_bid; // 买一价
         futureInfo.last = marketData.data[0].last; // 最新成交价
-        console.log(tableType, futureInfo);
     } else if (tableType === 'swap/ticker') {
       // 行情数据
       swapInfo.best_ask = marketData.data[0].best_ask; // 卖一价
       swapInfo.best_bid = marketData.data[0].best_bid; // 买一价
       swapInfo.last = marketData.data[0].last; // 最新成交价
-      console.log(tableType, swapInfo);
     } else if (tableType === 'swap/funding_rate') {
       fundingInfo.estimated_rate = marketData.data[0].estimated_rate;
       fundingInfo.funding_rate = marketData.data[0].funding_rate;
       fundingInfo.funding_time = moment(marketData.data[0].funding_time).format('YYYY-MM-DD HH:mm:ss');
       fundingInfo.settlement_time = moment(marketData.data[0].settlement_time).format('YYYY-MM-DD HH:mm:ss');
-      console.log(fundingInfo);
     } else {
       console.log(marketData.data);
     }
 }
 
-setTimeout(async () => {
+setInterval(async () => {
   const priceDiff = futureInfo.last - swapInfo.last;
   const priceDiffPercent = swapInfo.last && 100 * priceDiff / swapInfo.last;
-  console.log('差价: ' + priceDiff.toFixed(2), '差价百分比：' + priceDiffPercent.toFixed(2));
   let text = `
-    BTC-USDT-210326 - SWAP:
-    差价：${priceDiff.toFixed(2)}
-    差价百分比：${priceDiffPercent.toFixed(2)}
-    资金费率：
-    ${fundingInfo.funding_time}：${fundingInfo.funding_rate}
-    ${fundingInfo.settlement_time}：${fundingInfo.estimated_rate}
+${symbol}-${future}: ${futureInfo.last}
+${symbol}-永续: ${swapInfo.last}
+差价：${priceDiff.toFixed(2)}
+差价百分比：${priceDiffPercent.toFixed(2)}
+资金费率：${fundingInfo.funding_rate}
+下一期预期资金费率：${fundingInfo.estimated_rate}
   `;
-  await dingding(text, config.dingdingToken);
-}, 5000);
+  console.log(text);
+}, 10000);
